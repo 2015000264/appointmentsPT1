0e259681396ad0d69e1e7e98f7aea60cccc1d75e
DavidCSIT <david.cartman@sit.ac.nz>
2019-01-18 10:38:22 +1300

    first commit

---
+++ storage/logs/log-2019-01-17.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-01-17 01:05:40 --> EA\Engine\Types\Type: Invalid argument value provided (http:/index.php/appointments/index/57ad2d7ce032408d948583b176d5738a)
+ERROR - 2019-01-17 01:05:40 --> #0 C:\xampp\htdocs\appointments\application\controllers\Appointments.php(562): EA\Engine\Types\Type->__construct('http:/index.php...')
+#1 C:\xampp\htdocs\appointments\system\core\CodeIgniter.php(532): Appointments->ajax_register_appointment()
+#2 C:\xampp\htdocs\appointments\index.php(353): require_once('C:\\xampp\\htdocs...')
+#3 {main}
+ERROR - 2019-01-17 21:53:56 --> EA\Engine\Types\Type: Invalid argument value provided (http:/index.php/appointments/index/1280b9f3ea355ae03a99343240622fe1)
+ERROR - 2019-01-17 21:53:56 --> #0 H:\2 Papers\ITC511\htdocs\appointmentsPT1\application\controllers\Appointments.php(562): EA\Engine\Types\Type->__construct('http:/index.php...')
+#1 H:\2 Papers\ITC511\htdocs\appointmentsPT1\system\core\CodeIgniter.php(532): Appointments->ajax_register_appointment()
+#2 H:\2 Papers\ITC511\htdocs\appointmentsPT1\index.php(353): require_once('H:\\2 Papers\\ITC...')
+#3 {main}
